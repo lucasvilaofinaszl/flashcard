26555179c0e28b06c8ebb025103c736c28dc988c
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-10-13 09:20:16 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+function criarCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+}
